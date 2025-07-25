fb40191a46afc4ec8154507fbb5fee43a252a835
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2025-07-25 16:06:01 +0000

    Scaffold core module structure

---
+++ probechain.py
@@ -0,0 +1,14 @@
+# probechain.py
+"""
+Main module for ProbeChain application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
